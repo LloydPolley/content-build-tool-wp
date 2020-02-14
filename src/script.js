837ada76803c7457ed5c7eed2e49dd11c2420699
Lloyd Polley <lloydpolley@googlemail.com>
2020-02-14 11:07:26 +0000

    videos

--- src/script.js
+++ src/script.js
@@ -1,26 +1,35 @@
 (function() {
-  var allVideoContainer = document.querySelectorAll(".lp-video-container");
-  var allVideos = document.querySelectorAll(".lp-video");
-  console.log(allVideos);
+  var allVideoContainer = document.querySelectorAll(".lp-video");
+  var allVideos = document.querySelectorAll(".lp-video video");
 
   allVideos.forEach(video => {
     video.addEventListener("click", () => {
-      tapToPlayButton();
       clickToPlay(video);
     });
+
+    if (video.dataset.tapToPlay === true) {
+      addTapToPlayButton();
+    }
   });
 
   const clickToPlay = video => {
-    if (video.paused) {
-      video.play();
-      console.log("play");
-    } else {
-      video.pause();
-      console.log("pause");
+    for (let i = 0; i < allVideos.length; i++) {
+      if (allVideos[i] === video) {
+        if (video.paused) {
+          video.play();
+        } else {
+          video.pause();
+        }
+      } else {
+        allVideos[i].pause();
+      }
     }
   };
 
-  const tapToPlayButton = () =>{
-    
-  }
+  const addTapToPlayButton = container => {
+    const tapButton = '<p class="tap-to-play">Tap to play</div>';
+    tapButton.style.cssText =
+      "position: absolute; left: 50%; bottom: 25%; transform: translatex(-50%); color: white;";
+    container.innerHTML += tapButton;
+  };
 })();
