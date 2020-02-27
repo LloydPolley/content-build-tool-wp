8f3f20d6ff49a0415d53837dc17d9fd98da25ad7
Lloyd Polley <lloydpolley@googlemail.com>
2020-02-27 12:27:23 +0000

    read me

--- README.md
+++ README.md
@@ -1,5 +1,27 @@
 # content-build-tool-wp
+Run 'npm install' to install everything to get it working
+
+# Development
+Run 'npm run start' for normal development
+
+# Production
+Run 'npm run build' to produce the dist folder with all the files needed to copy over to the cms
+
+# Production watch
+Run npm run watch - this does the same as "npm run build" but watches it so it updates all the time - will be slowe
 
 
-# Dist
-Run 'npm run build' to product the dist folder with all the files needed to copy over to the cms
+
+# Entry points
+Inside src/components/entry.js - this contains all the the files which are being used for the page in development - you can add and remove them here by importing
+
+
+# Region Selector 
+You can change the region inside - src/config/index.js - the target region class adder
+
+
+# Problems
+No Jquery yet
+Nested mixins in media queries causing problems
+
+
--- src/script.js
+++ src/script.js
@@ -1,35 +1,129 @@
-(function() {
-  var allVideoContainer = document.querySelectorAll(".lp-video");
-  var allVideos = document.querySelectorAll(".lp-video video");
+class fixedImage {
+  constructor(
+    image1,
+    image2,
+    image3,
+    target1,
+    target2,
+    target3,
+    endTarget,
+    container,
+    offSetFadeIn,
+    pageAnchors
+  ) {
+    this.image1 = image1;
+    this.image2 = image2;
+    this.image3 = image3;
+    this.target1 = target1;
+    this.target2 = target2;
+    this.target3 = target3;
+    this.endTarget = endTarget;
+    this.container = container;
+    this.offSetFadeIn = offSetFadeIn;
+    this.pageAnchors = pageAnchors;
+    this.init();
+  }
 
-  allVideos.forEach(video => {
-    video.addEventListener("click", () => {
-      clickToPlay(video);
-    });
+  handleOpacity(image, target, pAnchor) {
+    let middle =
+      target.getBoundingClientRect().top -
+      window.innerHeight / 2 +
+      this.offSetFadeIn;
 
-    if (video.dataset.tapToPlay === true) {
-      addTapToPlayButton();
-    }
-  });
-
-  const clickToPlay = video => {
-    for (let i = 0; i < allVideos.length; i++) {
-      if (allVideos[i] === video) {
-        if (video.paused) {
-          video.play();
-        } else {
-          video.pause();
-        }
-      } else {
-        allVideos[i].pause();
+    if (window.innerWidth > 701) {
+      if (middle < 0) {
+        pAnchor.style.display = 'block';
+        image.style.opacity = 1;
+      } else if (middle > 0) {
+        pAnchor.style.display = 'none';
+        image.style.opacity = 0;
+      }
+    } else {
+      if (middle < 0) {
+        image.style.opacity = 1;
+      } else if (middle > 0) {
+        image.style.opacity = 0;
       }
     }
-  };
 
-  const addTapToPlayButton = container => {
-    const tapButton = '<p class="tap-to-play">Tap to play</div>';
-    tapButton.style.cssText =
-      "position: absolute; left: 50%; bottom: 25%; transform: translatex(-50%); color: white;";
-    container.innerHTML += tapButton;
-  };
-})();
+    if (this.endTarget) {
+      if (this.endTarget.getBoundingClientRect().top - 500 < 0) {
+        this.container.style.opacity = 0;
+      } else {
+        this.container.style.opacity = 1;
+      }
+    }
+  }
+
+  init() {
+    window.addEventListener("scroll", () => {
+      // console.log(this.pageAnchors)
+      if (window.innerWidth > 701) {
+        this.handleOpacity(this.image2, this.target2, this.pageAnchors[1]);
+        this.handleOpacity(this.image3, this.target3, this.pageAnchors[2]);
+      } else {
+        this.handleOpacity(this.image2, this.target2);
+        this.handleOpacity(this.image3, this.target3);
+      }
+    });
+  }
+}
+
+const desktopImages = document.querySelectorAll(".prm-fixed img");
+const desktopAnchor = document.querySelectorAll(".desktop-anchor");
+const endAnchor = document.querySelectorAll(".end-target")[0];
+const containerDesk = document.querySelectorAll(".prm-fixed")[0];
+
+const pageAnchors = document.querySelectorAll(".page-anchors");
+
+//Desktop
+if (window.innerWidth > 1025) {
+  const desktopFixed = new fixedImage(
+    desktopImages[0],
+    desktopImages[1],
+    desktopImages[2],
+    desktopAnchor[0],
+    desktopAnchor[1],
+    desktopAnchor[2],
+    endAnchor,
+    containerDesk,
+    0,
+    pageAnchors
+  );
+}
+
+//Tablets
+if (window.innerWidth > 701 && window.innerWidth < 1025) {
+  const desktopFixed = new fixedImage(
+    desktopImages[0],
+    desktopImages[1],
+    desktopImages[2],
+    desktopAnchor[0],
+    desktopAnchor[1],
+    desktopAnchor[2],
+    endAnchor,
+    containerDesk,
+    300,
+    pageAnchors
+  );
+}
+
+const mobImages = document.querySelectorAll(".hero-mob img");
+const mobAnchor = document.querySelectorAll(".mob-anchor");
+const endAnchorMob = document.querySelectorAll('.end-target-mob')[0];
+const containerMob = document.querySelectorAll(".hero-mob")[0];
+
+//Mobile
+if (window.innerWidth < 701) {
+  const mobFixed = new fixedImage(
+    mobImages[0],
+    mobImages[1],
+    mobImages[2],
+    mobAnchor[0],
+    mobAnchor[1],
+    mobAnchor[2],
+    endAnchorMob,
+    containerMob,
+    -200
+  );
+}
